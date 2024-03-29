bf62d1fc7cf283312ef6c55e68315094928b39a0
Ashutosh Rana <ashutoshrana978@gmail.com>
2024-03-29 19:11:42 +0530

    changes done

---
+++ lib/ui/utils/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color grey = Color(0xFF7C828A);
+  static const Color blue = Color(0xFF147AD6);
+  static const Color disabled = Color(0x53559B2D); // Semi-transparent black
+  static const Color black = Color(0xFF000000);
+  static const Color green = Color(0xFF3CD942);
+  static const Color red = Color(0xFFEC6666);
+  static const Color  lightShadeCyan = Color(0xFF79D2DE);
+  static const Color darkBlueColor = Color(0xFF171D3C);
+}
